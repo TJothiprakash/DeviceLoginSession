6ab4d5f73aee73353b172d4ecc94001f6dbf4fcf
JP <jothiprakash888@gmail.com>
2025-09-04 20:06:14 +0530

    Initial commit : Spring setup with local my-sql setup

---
+++ src/main/java/com/jp/dbbasedsession/DbbasedsessionApplication.java
@@ -0,0 +1,17 @@
+package com.jp.dbbasedsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+
+@SpringBootApplication
+public class DbbasedsessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbbasedsessionApplication.class, args);
+        System.out.println("Hello World!!! !!!! ! ! !  ! ! !");
+    }
+
+}
